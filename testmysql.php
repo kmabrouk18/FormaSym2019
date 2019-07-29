cd35cf965c6f19db4cf9d90594a30bcadaf94248
Khalil Mabrouk <khlil.mabrouk@gmail.com>
2019-07-29 13:31:06 +0100

    fichier testmysql : Erreur de connection

--- testmysql.php
+++ testmysql.php
@@ -7,7 +7,7 @@
 $mysqli = new mysqli('127.0.0.1', 'root', '');
 
 if ($mysqli->connect_error) {
-    die('Connect Error (' . $mysqli->connect_errno . ') '
+    die('Erreur de connection (' . $mysqli->connect_errno . ') '
             . $mysqli->connect_error);
 }
 echo 'Connection OK';
